50ba0b4765a3137ffa32cce05879755e91e0b863
TheBraveChicken4 <josh@caboclan.com>
2024-02-21 00:14:08 -0500

    Added Guild token
    Got the program to compile. Connected proper token from a .env file and got the guild name imported along with ID

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ from dotenv import load_dotenv
 #First we need to connect to a client
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+# TOKEN = "MTIwODQ1Mzc4NzYyMzA5NjM2MA.GNZSUE.AACEioTT4lAanvlc986os64t0vLDgODK6lqCW0"
 
-client = discord.Client(intents=discord.Intents.all())
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name} (id: {guild.id})'
+    )
 client.run(TOKEN)
