21b9576b2175d297353171a604ad84bd5e3f68bd
TheBraveChicken4 <josh@caboclan.com>
2024-02-22 11:49:08 -0500

    Added message responses
    Practiced messing around with message responses as well as messaging new members. I will work on using ML or another API to connect to this process and make the message response a lot more general.

--- main.py
+++ main.py
@@ -6,20 +6,41 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-# TOKEN = "MTIwODQ1Mzc4NzYyMzA5NjM2MA.GNZSUE.AACEioTT4lAanvlc986os64t0vLDgODK6lqCW0"
 
 intents = discord.Intents.default()
 intents.members = True
+intents.message_content = True
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    guild = discord.utils.get(client.guilds, name=GUILD)
 
     print(
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name} (id: {guild.id})'
     )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content == "Hello":
+        await message.channel.send("Hello!")
+
+    if message.content.startswith('!When do we meet?'):   
+        await message.channel.send('We meet every other Monday from 7-8pm. Our meeting schedule is on our website.')
+
+
 client.run(TOKEN)
